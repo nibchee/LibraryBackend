0f9c74cedf2f524db913769cf0b02590b38fc23b
nibchee <nirbhaybansal28@gmail.com>
2023-07-07 02:40:20 +0530

    AdminQuestionRequest Model Creation

--- src/main/java/com/nirbhay/springbootlibrary/dao/MessageRepository.java
+++ src/main/java/com/nirbhay/springbootlibrary/dao/MessageRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
-public interface MessageRepository extends JpaRepository<Message,Long> {
-    Page<Message> findByUserEmail(@RequestParam("userEmail")String userEmail, Pageable pageable);
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    Page<Message> findByUserEmail(@RequestParam("userEmail") String userEmail, Pageable pageable);
+
+    Page<Message> findByClosed(@RequestParam("closed") boolean closed, Pageable pageable);
 }
---
+++ src/main/java/com/nirbhay/springbootlibrary/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,12 @@
+package com.nirbhay.springbootlibrary.requestmodels;
+
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+}
